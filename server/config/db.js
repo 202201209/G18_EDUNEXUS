c7ebe80e99278c3698fd2f23f363a32704a2d0bb
202201228 <202201228@daiict.ac.in>
2024-12-02 01:14:24 +0530

    corrections in backend

--- server/config/db.js
+++ server/config/db.js
@@ -9,32 +9,12 @@ const pool = new Pool({
       rejectUnauthorized: require  // This allows the connection without certificate validation
     }
   });
-// pool
-//   .connect()
-//   .then(() => console.log("Connected to PostgreSQL"))
-//   .catch((err) => {
-//     console.error("Error connecting to PostgreSQL:", err.message);
-//     process.exit(1);
-//   });
+pool
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => {
+    console.error("Error connecting to PostgreSQL:", err.message);
+    process.exit(1);
+  });
 
   module.exports = pool;
-
-
-
-// const { config } = require("dotenv");
-// const { Pool } = require("pg");
-// require("dotenv").config();
-// config();
-
-// const pool = new Pool({
-
-//   user: "postgres",
-//   password: "Jaimin@123",
-//   database: "EduNexus",
-//   host: "localhost",
-//   port: 5432,
-// //   connectionString: process.env.DATABASE_URL,
-// //   ssl: true, 
-// });
-
-//   module.exports = pool;
